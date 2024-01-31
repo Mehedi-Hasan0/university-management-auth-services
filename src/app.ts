f3f131a1e46424f7373d4cd86740a950397cd4af
Mehedi-Hasan0 <developermahedi.me@gmail.com>
2024-01-31 19:39:28 +0600

    Testing

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,17 @@
-import express, { Application, Request, Response } from "express";
-import cors from "cors";
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
 
-const app: Application = express();
+const app: Application = express()
 
-app.use(cors());
+app.use(cors())
 
 // parse data
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Server running on port 5000");
-});
+// Testing
+app.get('/', (req: Request, res: any) => {
+  res.send('Server running on port 5000')
+})
 
-export default app;
+export default app
